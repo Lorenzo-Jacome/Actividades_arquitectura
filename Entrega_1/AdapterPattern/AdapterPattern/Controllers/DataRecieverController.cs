2ad0d1b6fab052fe46b1571e2b0061219fc860b3
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2022-11-13 21:43:48 -0600

    Entregable 1

---
+++ Entrega_1/AdapterPattern/AdapterPattern/Controllers/DataRecieverController.cs
@@ -0,0 +1,27 @@
+﻿using AdapterPattern.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Linq;
+using AdapterPattern.Adapters;
+
+namespace AdapterPattern.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataRecieverController : ControllerBase
+    {
+        //Función que recibe peticiones POST en JSON y las regresa en XML:
+        [HttpPost]
+        public string DataReciever(PersonModel personData)
+        {
+            string personDataJson = Newtonsoft.Json.JsonConvert.SerializeObject(personData);
+
+            XmlAdapter adaptador = new XmlAdapter();
+
+            string xmlAdaptedData = adaptador.JsonToXmlAdapter(personDataJson);
+
+            return xmlAdaptedData.ToString();
+        }
+    }
+}
---
+++ Entrega_1/AdapterPattern/AdapterPattern/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AdapterPattern.Models
+{
+    public class PersonModel
+    {
+        public string Nombre    { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int Semestre { get; set; }
+        public string Materia { get; set; }
+    }
+}
